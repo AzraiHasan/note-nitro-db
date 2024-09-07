13d5e022d05d22cfd11ce622d2ed3ac9e5fdc532
AjaeOnTheRun <syahrul.azrai@gmail.com>
2024-09-07 16:04:43 +0800

    kinde not working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,12 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/kinde"],
+  /* kinde: {
+    domain: process.env.NUXT_KINDE_AUTH_DOMAIN,
+    clientId: process.env.NUXT_KINDE_CLIENT_ID,
+    clientSecret: process.env.NUXT_KINDE_CLIENT_SECRET,
+    redirectUri: process.env.NUXT_KINDE_REDIRECT_URL,
+    logoutRedirectUri: process.env.NUXT_KINDE_LOGOUT_REDIRECT_URL,
+    postLoginRedirectURL: process.env.NUXT_KINDE_POST_LOGIN_REDIRECT_URL
+  } as any, */
 });
