0492029f19bda5ab4c54637d163c35cbd11ab90a
AjaeOnTheRun <syahrul.azrai@gmail.com>
2024-09-07 15:26:51 +0800

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  nitro: {
-    experimental: {
-      database: true
-    },
-    database: {
-      default: {
-        connector: 'sqlite',
-        options: { name: 'db' }
-      }
-    }
-  }
-})
+  modules: ["@nuxtjs/kinde"],
+});
