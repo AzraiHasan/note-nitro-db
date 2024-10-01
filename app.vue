0006c5f65098a58778dd7e3598339fc4c06e271a
AjaeOnTheRun <syahrul.azrai@gmail.com>
2024-10-01 13:52:19 +0800

    init

--- app.vue
+++ app.vue
@@ -1,23 +1,11 @@
-<script setup lang="ts">
-  const nameInput = ref<any>('');
-  const emailInput = ref<any>('');
-
-  function submit () {
-    $fetch('/api/addUser', {
-      method: 'POST',
-      body: {
-        name: nameInput.value,
-        email: emailInput.value
-      }
-    })
-  }
-</script>
-
 <template>
   <div>
-    <input v-model="nameInput" type="text" placeholder="Name">
-    <input v-model="emailInput" type="text" placeholder="Email">
-    <br>
-    <button @click="submit">Add</button>
+    hello nitro db
   </div>
 </template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
